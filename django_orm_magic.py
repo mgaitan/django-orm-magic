2779f889e607d7a50995cccc3ea45ad34c74c5aa
Martín Gaitán <gaitan@gmail.com>
2015-06-26 15:48:29 -0300

    hot fix to work with dj18

--- django_orm_magic.py
+++ django_orm_magic.py
@@ -1,42 +1,34 @@
-import importlib
 import sys
 import os.path
 import tempfile
 
+import django
+from django.conf import settings
 from django.core.management import call_command
 from django.utils.crypto import get_random_string
 from IPython.core import magic_arguments
 from IPython.core.magic import Magics, magics_class, cell_magic, line_cell_magic
+from django.utils import six
+
+if six.PY3:
+    from importlib import reload
 
 
 chars = 'abcdefghijklmnopqrstuvwxyz0123456789'
 
-secret_key = """
-SECRET_KEY = '%s'
-""" % get_random_string(50, chars)
-
-settings_default = """
-DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': 'db.sqlite' #':memory:'
-    }
-  }
-INSTALLED_APPS = ("orm_magic",)
-"""
+DEFAULT_SETTINGS = {'SECRET_KEY': get_random_string(50, chars),
+                    'DATABASES': {
+                        'default': {
+                            'ENGINE': 'django.db.backends.sqlite3',
+                            'NAME': 'db.sqlite' #':memory:'
+                        }
+                    },
+                    'INSTALLED_APPS': ("orm_magic",)
+                    }
 
 
 @magics_class
 class DjangoOrmMagics(Magics):
-
-    def get_settings(self, add_secret_key=False):
-        try:
-            settings = self.shell.db['django_orm']
-        except KeyError:
-            settings = settings_default
-        if add_secret_key:
-            settings = secret_key + settings
-        return settings
 
     def _import_all(self, module, verbosity=0):
         imported = []
@@ -48,71 +40,41 @@ class DjangoOrmMagics(Magics):
             print("\nOk. The following django models"
                   "are ready to use: %s" % ", ".join(imported))
 
-    @magic_arguments.magic_arguments()
-    @magic_arguments.argument(
-        '--default', action="store_true", help="Delete custom settings "
-        "and back to the default"
-    )
-    @line_cell_magic
-    def django_settings(self, line, cell=""):
-        """
-        Show and setup the django settings to use with %%django_orm
-
-            %django_settings
-
-                Load the current custom configuration in a new cell
-
-            %django_settings --default
-
-                Delete the current configuration and back to the default
-
-            %%django_settings
-
-            DATABASES = {
-              'default': {
-                'ENGINE': 'django.db.backends.sqlite3',
-                'NAME': 'db.sqlite' #':memory:'
-                }
-              }
-            INSTALLED_APPS = ("orm_magic",)
-        """
-        args = magic_arguments.parse_argstring(self.django_settings, line)
-        if args.default:
-            try:
-                del self.shell.db['django_orm']
-                print("Deleted custom settings. Back to default for %%django_orm")
-            except KeyError:
-                print("No custom settings found for %%django_orm")
-        elif not line and not cell:
-            self.shell.set_next_input("%%django_settings\n" + self.get_settings())
-        else:
-            self.shell.db['django_orm'] = cell
-            print("Settings for %%django_orm configured succesfully")
-
     @cell_magic
     def django_orm(self, line, cell):
         j = os.path.join
+        if not settings.configured:
+            try:
+                del self.shell.db['orm_magic_path']
+            except KeyError:
+                pass
+            settings.configure(**DEFAULT_SETTINGS)
+            temp_project_path = self.shell.db['orm_magic_path'] = tempfile.mkdtemp()
+            sys.path.append(temp_project_path)
+            tmp, temp_project = os.path.split(temp_project_path)
 
-        temp_project_path = tempfile.mkdtemp()
-        tmp, temp_project = os.path.split(temp_project_path)
+            temp_dir = j(temp_project_path, 'orm_magic')
+            os.makedirs(temp_dir)
+            open(j(temp_project_path, '__init__.py'), 'a').close()
+            open(j(temp_dir, '__init__.py'), 'a').close()
 
-        temp_dir = j(temp_project_path, 'orm_magic')
-        os.makedirs(temp_dir)
-        open(j(temp_project_path, '__init__.py'), 'a').close()
-        open(j(temp_dir, '__init__.py'), 'a').close()
+        else:
+            temp_project_path = self.shell.db['orm_magic_path']
+            temp_dir = j(temp_project_path, 'orm_magic')
 
         module_path = j(temp_dir, 'models.py')
         with open(module_path, 'w') as fh:
-            fh.write(cell)
-        with open(j(temp_dir, 'settings.py'), 'w') as fh:
-            fh.write(self.get_settings(True))
+            fh.write('\n' + cell)
 
-        sys.path.append(temp_project_path)
-        os.environ['DJANGO_SETTINGS_MODULE'] = 'orm_magic.settings'
-        call_command('syncdb', verbosity=0, interactive=False)
+        django.setup()
+        call_command('makemigrations', 'orm_magic', verbosity=0, interactive=False)
+        call_command('migrate', 'orm_magic', verbosity=0, interactive=False)
 
-        module = importlib.import_module('orm_magic.models', temp_project)
-        self._import_all(module)
+        try:
+            reload(orm_magic_models)
+        except UnboundLocalError:
+            from orm_magic import models as orm_magic_models
+        self._import_all(orm_magic_models)
         sys.path.pop()
 
 
